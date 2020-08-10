6ab58a3910f7b9fb4ea4826a6a30bc36bbdf1c0e
retropikzel <retropikzel@mailbox.org>
2020-08-10 14:55:51 +0300

    [FIX] Model

--- account_financial_report_account_analytic/models/account_financial_report.py
+++ account_financial_report_account_analytic/models/account_financial_report.py
@@ -3,7 +3,7 @@ from odoo import api, models, fields
 
 
 class AccountCommonReport(models.TransientModel):
-    _inherit = "account.common.report"
+    _inherit = "accounting.report"
 
     account_analytic = fields.Many2one(
         comodel_name='account.analytic.account',
@@ -11,24 +11,3 @@ class AccountCommonReport(models.TransientModel):
         copy=False,
         string='Account analytic',
     )
-
-    def _build_contexts(self, data):
-        result = {}
-        result['journal_ids'] = 'journal_ids' in data['form'] and data['form']['journal_ids'] or False
-        result['state'] = 'target_move' in data['form'] and data['form']['target_move'] or ''
-        result['date_from'] = data['form']['date_from'] or False
-        result['date_to'] = data['form']['date_to'] or False
-        result['strict_range'] = True if result['date_from'] else False
-        result['analytic_account'] = data['account_analytic'] or False
-        return result
-
-    @api.multi
-    def check_report(self):
-        self.ensure_one()
-        data = {}
-        data['ids'] = self.env.context.get('active_ids', [])
-        data['model'] = self.env.context.get('active_model', 'ir.ui.menu')
-        data['form'] = self.read(['date_from', 'date_to', 'journal_ids', 'target_move', 'analytic_account'])[0]
-        used_context = self._build_contexts(data)
-        data['form']['used_context'] = dict(used_context, lang=self.env.context.get('lang') or 'en_US')
-        return self._print_report(data)
